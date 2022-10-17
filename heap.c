ad590a99d0c9c69224d1c5d3c1ec847f90d832b0
Replit user <>
2022-10-17 00:53:46 +0000

    new try

--- heap.c
+++ heap.c
@@ -54,32 +54,6 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
-  heapElem* aux = (heapElem*)malloc(sizeof(heapElem)); 
-  heapElem* mayor = (heapElem*)malloc(sizeof(heapElem));
-  *aux = pq->heapArray[0];
-
-  
-  aux = pq->heapArray[pq->size];
-  pq->heapArray[pq->size] = pq->heapArray[0];
-  pq->heapArray[0] = aux;
-
-  pq->heapArray[pq->size].data = NULL;
-  pq->heapArray[pq->size].priority = 0;
-  pq->size--;
-
-  mayor.data = pq->heapArray[0].data;
-  mayor.priority = pq->heapArray[0].priority;
-  int i;
-  while(i != pq->capac){
-
-    if (mayor.priority <= pq->heapArray[i].priority){
-      aux = mayor;
-      mayor = pq->heapArray[i];
-      pq->heapArray[i] = aux;
-    }
-
-    i++;
-  }
   }
 
 Heap* createHeap(){
